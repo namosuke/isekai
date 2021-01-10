4c152a723feac74bc9b0ff70452d3ccd9087bebb
ナモすけ <dynamorollerbot@gmail.com>
2021-01-11 04:34:18 +0900

    なんとかログインできる

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,14 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # メール設定
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sfc.keio.ac.jp',
+    :port => 25,
+    :domain => 'sfc.keio.ac.jp'
+  }
 end
